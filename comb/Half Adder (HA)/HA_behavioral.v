652b2518933994c029fbef5d46cf1c3807dfb154
ksskalikrishna <kalikondaveeti@gmail.com>
2026-08-21 17:59:47 +0530

    Delete comb/Half Adder (HA) directory

--- comb/Half Adder (HA)/HA_behavioral.v
+++
@@ -1,8 +0,0 @@
-module HA(
-input [7:0] a,b,
-output reg [7:0] s,
-output reg co);
-always @(*) begin
-{co,s}=a+b;
-end
-endmodule
--- comb/Half Adder (HA)/HA_dataflow.v
+++
@@ -1,6 +0,0 @@
-module HA(
-input [7:0] a,b,
-output [7:0] s,
-output co);
-assign {co,s}=a+b;
-endmodule
